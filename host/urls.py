06bd7b84f364a87c9fc1a81e0655d9789c5718e1
Jin Chengyu <monday500jcy@gmail.com>
2023-06-15 21:00:07 +0900

    Added two customer data for testing(ID: tom, Ada), added listcustomers function to list all customers, added listcustomerByPhoneNum function to list customer by phone number.

--- host/urls.py
+++ host/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from host.views import listorders
+from host.views import listcustomers
+from host.views import listcustomerByPhoneNum
 
 # Router patterns: URLとcallされる関数のbind。
 urlpatterns = [
     # My binds:
     path('orders/', listorders),
+    path('customers/', listcustomers),
+    path('customerByPhoneNum/', listcustomerByPhoneNum),
 
 ]
--- host/views.py
+++ host/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from common.models import Customer
+
 
 # Create your views here.
 def listorders(request):
     return HttpResponse("下面是系统中所有的订单信息。。。二级表接口实现")
+
+
+#
+def listcustomers(request):
+    # 返回一个 QuerySet 对象 ，包含所有的表记录
+    # 每条表记录都是是一个dict对象，
+    # key 是字段名，value 是 字段值
+    qs = Customer.objects.values()
+
+    # 定义返回字符串
+    retStr = str()
+    for customer in qs:
+        for name, value in customer.items():
+            retStr += f'{name} : {value} | '
+
+        # <br> 表示换行
+        retStr += '<br>'
+
+    return HttpResponse(retStr)
+
+
+#
+def listcustomerByPhoneNum(request):
+    # 返回一个 QuerySet 对象 ，包含所有的表记录
+    qs = Customer.objects.values()
+
+    # 检查url中是否有参数phonenumber
+    ph = request.GET.get('phoneNumber', None)
+
+    # 如果有，添加过滤条件
+    if ph:
+        qs = qs.filter(phoneNumber=ph)
+
+    # 定义返回字符串
+    retStr = str()
+    for customer in qs:
+        for name, value in customer.items():
+            retStr += f'{name} : {value} | '
+        # <br> 表示换行
+        retStr += '<br>'
+
+    return HttpResponse(retStr)
